9f970bf4f69bb1a8d09145ce5723ebd72b0d701f
Ian Thomas <ian@spectrumware.net>
2024-11-13 16:34:28 -0600

    enhance: add exported functions for mounting/unmounting react app

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,31 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot, Root } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Toaster } from "@/components/ui/sonner"
 import { QueryClient, QueryClientProvider } from 'react-query'
 
-createRoot(document.getElementById('cis-dashboard')!).render(
-  <StrictMode>
-    <QueryClientProvider client={new QueryClient}>
-      <App />
-      <Toaster closeButton />
-    </QueryClientProvider>
-  </StrictMode>,
-)
+let appRoot: Root | null = null
+
+export function mountApp(element: HTMLElement) {
+  console.log(`Mounting dashboard app in <div> with id: "${element.id}"`)
+  appRoot = createRoot(element)
+  
+  appRoot.render(
+    <StrictMode>
+      <QueryClientProvider client={new QueryClient}>
+        <App />
+        <Toaster closeButton />
+      </QueryClientProvider>
+    </StrictMode>
+  )
+}
+
+export function unmountApp() {
+  if (appRoot) {
+    console.log("Unmounting dashboard app")
+    appRoot.unmount()
+  }
+}
+
+
